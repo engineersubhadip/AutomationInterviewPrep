ee0875625e90acb4d81f1a63e64076a0e6a8e4c8
Subhadip Das <subhadipdas911@gmail.com>
2025-01-22 20:39:13 +0530

    Explained as to how singleton and Thread Local will help in Multi-Threaded and SingleThreaded Test Execution

--- Design_Patterns/src/test/java/SingletonDesign/Test2.java
+++ Design_Patterns/src/test/java/SingletonDesign/Test2.java
@@ -4,11 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test2 extends BaseClass {
-	
+
 	@Test
-	public void testCase2 () {
+	public void testCase2() {
 		try {
-			System.out.println(driver+" from test case 2");
+			System.out.println(driver + " from test case 2");
 			Thread.sleep(3500);
 			System.out.println(driver.getTitle());
 			Assert.assertTrue(true);
