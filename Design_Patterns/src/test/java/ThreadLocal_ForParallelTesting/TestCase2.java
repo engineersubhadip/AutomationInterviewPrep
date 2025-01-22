801173e706625bad890b9813d4e3bba593b9cdf2
Subhadip Das <subhadipdas911@gmail.com>
2025-01-22 11:50:13 +0530

    How Parallel Testing for Concurrent Threads, using independent driver instance

---
+++ Design_Patterns/src/test/java/ThreadLocal_ForParallelTesting/TestCase2.java
@@ -0,0 +1,21 @@
+package ThreadLocal_ForParallelTesting;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCase2 extends BaseClass {
+	
+	@Test
+	public void testCase2 () {
+		try {
+			System.out.println(driver);
+			driver.navigate().to("https://www.udemy.com/");
+			Thread.sleep(3500);
+			System.out.println(driver.getTitle());
+			Assert.assertTrue(true);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			Assert.fail();
+		}
+	}
+}
