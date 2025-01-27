c50d16367e0468770cbec5544e08401eab478b6c
Subhadip Das <subhadipdas911@gmail.com>
2025-01-27 13:47:45 +0530

    Added the WebElement to redirect to Contact Us Page

--- AutomationExercise/src/test/java/pageObjects/HomePage.java
+++ AutomationExercise/src/test/java/pageObjects/HomePage.java
@@ -12,33 +12,40 @@ public class HomePage extends BasePage {
 
 	@FindBy(xpath = "//a[contains(text(),'Signup')]")
 	WebElement signUpLoginLink;
-	
-	@FindBy(xpath="//a[contains(@href,'delete')]/i")
+
+	@FindBy(xpath = "//a[contains(@href,'delete')]/i")
 	WebElement deleteAccountLink;
-	
-	@FindBy(xpath="//ul[@class='nav navbar-nav']/li[last()]")
+
+	@FindBy(xpath = "//ul[@class='nav navbar-nav']/li[last()]")
 	WebElement loggedInUser;
-	
-	@FindBy(xpath="//a[contains(@href,'logout')]")
+
+	@FindBy(xpath = "//a[contains(@href,'logout')]")
 	WebElement logoutLink;
-	
+
+	@FindBy(xpath = "//a[contains(@href,'contact')]")
+	WebElement contactUsLink;
+
+	public void clickContactUsLink() {
+		this.contactUsLink.click();
+	}
+
 	public void clickSignupLoginLink() {
 		this.signUpLoginLink.click();
 	}
-	
+
 	public void clickLogoutLink() {
 		this.logoutLink.click();
 	}
-	
+
 	public void clickDeleteAccountButton() {
 		this.deleteAccountLink.click();
 	}
-	
+
 	public boolean checkUserLoginStatus(String userString) {
 		String currentValue = this.loggedInUser.getText();
 		currentValue = currentValue.trim();
 		userString = userString.trim();
-		if(currentValue.equals(userString)) {
+		if (currentValue.equals(userString)) {
 			return true;
 		}
 		return false;
